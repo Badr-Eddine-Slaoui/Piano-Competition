df675ac7d35c934a3bfec4b018673c95c6dcab83
Badr-Eddine-Slaoui <slaoui.badr.eddine.sdv@gmail.com>
2024-12-05 17:14:25 +0100

    Fix Err
    Fix Err

--- resources/views/Pages/Paris/accueil.blade.php
+++ resources/views/Pages/Paris/accueil.blade.php
@@ -41,10 +41,10 @@
           </div>
       </div>
     </section>
-    <section id="mot-de-le-president" class="w-full my-5 xs:min-h-[20vh] sm:min-h-[30vh] md:min-h-[40vh] lg:min-h-[50vh]">
+    <section id="mot-du-president" class="w-full my-5 xs:min-h-[20vh] sm:min-h-[30vh] md:min-h-[40vh] lg:min-h-[50vh]">
       <div class="grid grid-cols-1 grid-rows-1 xs:gap-y-10 xs:min-h-[20vh] sm:gap-y-16 sm:min-h-[30vh] sm:w-5/6 sm:ms-auto sm:me-8 md:min-h-[40vh] md:gap-y-32 lg:min-h-[65vh] lg:w-5/6 lg:ms-auto lg:me-14 lg:gap-y-40 xl:gap-y-52">
         <div class="flex relative z-[-2] items-center mt-10 slide-text xs:h-[5vh] sm:h-[20vh]">
-          <h2 class="font-[400] font-Against text-center text-gold xs:text-[14px] sm:text-[24px] md:text-[28px] lg:text-[32px] xl:text-[36px]">Mot de le président</h2>
+          <h2 class="font-[400] font-Against text-center text-gold xs:text-[14px] sm:text-[24px] md:text-[28px] lg:text-[32px] xl:text-[36px]">Mot du président</h2>
         </div>
         <div class="message relative xs:min-h-[40vh] xs:mb-10 md:mb-0 lg:min-h-[65vh]">
           <i class="fa-solid fa-quote-left absolute xs:text[30px] xs:-top-5 xs:left-2 sm:text-[35px] sm:-top-10 md:text-[40px] lg:text-[45px] xl:text-[60px] xl:-top-10 xl:left-0"></i>
